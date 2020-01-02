dc3792c71684a24d3a4458be9d7b153b4fa3b9bd
Kato Hiroki <k.hiro1818@gmail.com>
2020-01-02 09:23:53 +0900

    Add static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,12 @@ class StaticPagesController < ApplicationController
   def home
   end
 
+  def contest
+  end
+
   def world_tour
+  end
+
+  def links
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get '/home'      , to: 'static_pages#home'
+  get '/contest'   , to: 'static_pages#contest'
   get '/world_tour', to: 'static_pages#world_tour'
 end
