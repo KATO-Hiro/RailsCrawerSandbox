49470d1864c097d9d6e4f3068d7abe99fb8864d8
Kato Hiroki <k.hiro1818@gmail.com>
2020-01-08 08:43:08 +0900

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   get '/world_tour', to: 'static_pages#world_tour'
 
   get    '/signup', to: 'users#new'
-  get    '/login' , to: 'sessions/new'
-  post   '/login' , to: 'sessions/create'
-  delete '/logout', to: 'sessions/destroy'
+  get    '/login' , to: 'sessions#new'
+  post   '/login' , to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
