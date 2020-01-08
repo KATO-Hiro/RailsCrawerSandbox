f0a46a6819cefff9e039e35afd3523310d826549
Kato Hiroki <k.hiro1818@gmail.com>
2020-01-08 10:57:41 +0900

    Enable to handle invalid input in login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+
+    if user && user.authenticate(params[:session][:password])
+      # redirect_to Something_path
+    else
+      flash.now[:danger] = 'Invalid name/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
 end
