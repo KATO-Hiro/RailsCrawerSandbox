54606ced4f50c4968961681840299165aad674c3
Kato Hiroki <k.hiro1818@gmail.com>
2020-02-12 13:25:02 +0900

    Create contest model

---
+++ db/migrate/20200212042218_create_contests.rb
@@ -0,0 +1,13 @@
+class CreateContests < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contests do |t|
+      t.string :name
+      t.bigint :start_epoch_second
+      t.bigint :duration_second
+      t.text :title
+      t.string :rate_change
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/contests.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :contest do
+    name { "MyString" }
+    start_epoch_second { "" }
+    duration_second { "" }
+    title { "MyText" }
+    rate_change { "MyString" }
+  end
+end
